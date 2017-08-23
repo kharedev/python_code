0d0ff2336c76a2548dead7b49a1a3352106e6916
Somesh Khare <get2khare@gmail.com>
2017-08-23 10:37:04 +0000

    pushing the file

--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 import collections
 print("Hello This is my first program")
 
-obj = collections.Counter([1,2,3,4,3,4,1,4,1,1,1,1,1])
+obj = collections.Counter(['1','2','3','4','3','4','1','4','1','1','1','1','1'])
+obj = collections.Counter([1,2,3,1])
 print("The obj is "+ str(obj))
 
 
